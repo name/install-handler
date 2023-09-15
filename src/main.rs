883a13e9ae42d14e1932f205d8e4dc403a3ffd67
Charlie <charlie@maddex.me>
2023-09-15 14:02:08 +0100

    add exe/msi install handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process::{ Command, ExitStatus };
 
 fn main() {
     // Access command-line arguments as a vector of strings
@@ -6,28 +7,56 @@ fn main() {
 
     // Check the number of arguments
     if args.len() != 3 {
-        println!("Usage: {} <package/executable> <install_parameters>", args[0]);
+        println!("Usage: {} <package/executable> <installer_args>", args[0]);
         return;
     }
 
     // Get the package and install parameters
-    let package = &args[1];
-    let install_parameters = &args[2];
+    let installer_path = &args[1];
+    let installer_args = &args[2];
 
     // Use pattern matching to determine the installer type
-    match package {
-        p if p.ends_with(".exe") => install_exe(p, install_parameters),
-        p if p.ends_with(".msi") => install_msi(p, install_parameters),
-        _ => println!("Package must be an executable or MSI file"),
+    let status = match installer_path {
+        p if p.ends_with(".exe") => install_exe(p, installer_args),
+        p if p.ends_with(".msi") => install_msi(p, installer_args),
+        _ => {
+            println!("Path must be an executable or MSI file");
+            return;
+        }
+    };
+
+    // Check the exit status of the installer
+    match status {
+        Ok(exit_status) => {
+            if exit_status.success() {
+                println!("Installer completed successfully.");
+            } else {
+                println!("Installer failed with exit code: {:?}", exit_status.code());
+                println!(
+                    "Error codes can be looked up at https://learn.microsoft.com/en-us/windows/win32/msi/error-codes"
+                );
+            }
+        }
+        Err(e) => {
+            println!("Error running installer: {:?}", e);
+        }
     }
 }
 
 // Install MSI function
-fn install_msi(package: &str, install_parameters: &str) {
-    println!("Installing MSI {} with parameters {}", package, install_parameters);
+fn install_msi(installer_path: &str, installer_args: &str) -> Result<ExitStatus, std::io::Error> {
+    println!("Installing MSI {} with parameters {}", installer_path, installer_args);
+    Command::new("msiexec")
+        .args(&["/i", installer_path, installer_args])
+        .spawn()
+        .and_then(|mut child| child.wait())
 }
 
 // Install EXE function
-fn install_exe(package: &str, install_parameters: &str) {
-    println!("Installing EXE {} with parameters {}", package, install_parameters);
+fn install_exe(installer_path: &str, installer_args: &str) -> Result<ExitStatus, std::io::Error> {
+    println!("Installing EXE {} with parameters {}", installer_path, installer_args);
+    Command::new(installer_path)
+        .args(installer_args.split_whitespace()) // Split installer_args by spaces
+        .spawn()
+        .and_then(|mut child| child.wait())
 }
